68277701cca999411f2f178ef7be4a17cec4bff7
disha00991 <directionstar86@gmail.com>
2022-04-26 00:25:44 -0400

    doesnt work

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,17 +1,34 @@
-let color = 'pink';
+let color = 'red';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %c', `color: ${color}`);
+  // chrome.tabs.query({}, function (tabs) {
+  //   tabs.forEach(tab => {
+  //     chrome.scripting.executeScript({ // Error in event handler: TypeError: Cannot read property 'executeScript' of undefined
+  //       target: { tabId: tab.id },
+  //       files: ["opencv.js", "utils.js"]
+  //     }, (injectionResults) => {
+  //       for (const frameResult of injectionResults)
+  //         console.log('Frame Title: ' + frameResult.result);
+  //     });
+  //   });
+  // });
 });
 
+
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.isTab) {
+  if (request.istab) {
+    // chrome.scripting.executeScript({ // Error in event handler: TypeError: Cannot read property 'executeScript' of undefined
+    //   target: { tabId: request.to },
+    //   files: ["utils.js"]
+    // });
     chrome.scripting.executeScript({ // Error in event handler: TypeError: Cannot read property 'executeScript' of undefined
       target: { tabId: request.to },
-      file: "contentScript.js"
+      files: ["contentScript.js"]
     });
-  }  
+  }
 });
 
 
